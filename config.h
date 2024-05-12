db6b066f14791863eb031f3eaa964fc5080b6ec9
Anonymous <anonymous.negation790@passinbox.com>
2024-05-12 20:41:46 +0800

    slstatus Rice: (config.h)

--- config.h
+++ config.h
@@ -66,5 +66,5 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
         /* function format          argument */
 				{ battery_perc,         "  %s%%",       "BAT0" },
-        { datetime,             "  %s ",         "%D  %A, %I:%M %p" }, /* Date time with this format: Day name YYYY-MM-DD 18:00:00 */
+        { datetime,             "  %s ",         "%D  %a, %I:%M %p" }, /* Date time with this format: Day name YYYY-MM-DD 18:00:00 */
 };
